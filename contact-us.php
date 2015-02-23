1e347d5cdc2da94208d7768d327f9ca721b02012
Vy Nguyen <vy.quail@gmail.com>
2015-02-23 23:37:19 +1000

    pages plus contact form

---
+++ contact-us.php
@@ -0,0 +1,125 @@
+<!doctype html>
+<head >
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="lib/bootstrap-3.3.2-dist/css/bootstrap.css"/>
+	<link href="lib/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="css/main.css"/>
+	<title> DMR: Computer Repair Service Sydney</title>
+</head>
+
+<body>
+<!-- header and navigation  -->
+<nav class="navbar navbar-default">
+	<div class="container">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="index.html"><img alt="Brand" src="img/DMClogo.png"></a>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav navbar-left">
+				<li><a href="index.html">HOME</a></li>
+				<li><a href="our-service.html">SERVICE</a></li>
+				<li><a href="our-service.html#prices">PRICES</a></li>
+				<li><a href="service-areas.html">SERVICE AREAS</a></li>
+				<li><a href="contact-us.php">CONTACT</a></li>
+			</ul>
+	       	<ul class="nav navbar-nav navbar-right">
+        		<li> <a class="a" href="#"> <img src="img/ph.png"/></a></li>
+      		</ul>
+	    </div><!-- /.navbar-collapse -->
+	  </div><!-- /.container-fluid -->
+	</div>
+</nav>
+<!-- end of Header and navigation-->
+
+<!-- Contact Us -->
+<div class="contact-us-full">
+<div class="container ">
+	<div class="container-fluid contact-us ">
+		<section class="form">
+			<h2> Tell Us What You Need </h2>
+			<form class="contactForm.html" name="contactform" method="post" action="lib/send_form_email.php">
+				<input class="form-control input" placeholder="Name*" type="text" name="first_name" maxlength="50" size="30">
+				<input class="form-control input" placeholder="Email Address*" type="text" name="email" maxlength="80" size="30">
+				<input class="form-control input" placeholder="Telephone Number" type="text" name="telephone" maxlength="30" size="30">
+				<textarea class="form-control input" placeholder="Message*" name="comments" maxlength="1000" cols="25" rows="4"></textarea>
+				<button class="sub" type="submit">    SEND    </button>
+			</form>
+		<!-- <form name="contactform" method="post" action="send_form_email.php">
+			<table width="450px">
+			
+			<tr>
+				<td valign="top">
+					<label for="first_name">First Name *</label>
+				</td>
+				<td valign="top">
+					<input  type="text" name="first_name" maxlength="50" size="30">
+				</td>
+			</tr>
+
+			<tr>
+				<td valign="top">
+					<label for="last_name">Last Name *</label>
+				</td>
+				<td valign="top">
+					<input  type="text" name="last_name" maxlength="50" size="30">
+				</td>
+			</tr>
+			<tr>
+				<td valign="top">
+					<label for="email">Email Address *</label>
+				</td>
+				<td valign="top">
+					<input  type="text" name="email" maxlength="80" size="30">
+				</td>
+			</tr>
+
+			<tr>
+				<td valign="top">
+					<label for="telephone">Telephone Number *</label>
+				</td>
+				<td valign="top">
+					<input  type="text" name="telephone" maxlength="30" size="30">
+				</td>
+			</tr>
+
+			<tr>
+				<td valign="top">
+					<label for="comments">Comments *</label>
+				</td>
+				<td valign="top">
+					<textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
+				</td>
+			</tr>
+
+			<tr>
+				<td colspan="2" style="text-align:center">
+					<input type="submit" value="Submit">   
+				</td>
+			</tr>
+		</table>
+		</form> -->
+		</section>
+		<section class="logoman-talk">
+			<img src="img/logoman-talk.png"/>
+		</section>
+</div>
+</div>
+</div>
+<!-- Footer -->
+
+	</div>
+	<script src="lib/jquery/dist/jquery.js"></script>
+	<script src="lib/bootstrap-3.3.2-dist/js/bootstrap.js"></script>
+	<script src="https://maps.googleapis.com/maps/api/js"></script>
+<!--END OF PRICES  -->
+</body>
---
+++ start-server.cmd
@@ -0,0 +1,2 @@
+start chrome "http://localhost:8000/index.html" 
+php -S localhost:8000
